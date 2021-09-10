9a84692aad6fcc70e5ab4f97ed28b7bfbeead075
lehnele <danil.hap@mail.ru>
2021-09-10 22:54:42 +0300

    Теперь всё точно по плану

--- DZ_1_Dogadkin_v11/Program.cs
+++ DZ_1_Dogadkin_v11/Program.cs
@@ -7,7 +7,7 @@ namespace pr_1
         static void Main(string[] args)
         {
             Console.Write("Введите значение x = ");
-            int x = int.Parse(Console.ReadLine())
+            int x = int.Parse(Console.ReadLine());
             Console.Write($"3*x^6 - 6*x^2 - 7 = {3 * Math.Pow(x,6) - 6 * Math.Pow(x,2)-7}");
         }
     }
